c81d8e258c8f8bd7bb3b35339b3aa043dcd3509b
Jarret Lovelace <jarretlovelace@icloud.com>
2024-08-08 14:53:00 -0600

    almost done

--- api/server.js
+++ api/server.js
@@ -1,6 +1,45 @@
 const express = require('express')
-
+const User = require('./users/model')
 const server = express()
+
+server.post('/api/users', (req, res) => {
+    User.insert(user)
+
+})
+
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+       res.json(users)
+    })
+    .catch(err => {
+        res.status(). json({
+            message: 'error getting users', 
+            err: err.message,
+            stack: err.stack, 
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist", 
+            })
+        }
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500). json({
+            message: 'error getting user', 
+            err: err.message,
+            stack: err.stack, 
+        })
+    })
+})
+
 
 server.get('/api/users', (req, res) => {
     res.json('users')
