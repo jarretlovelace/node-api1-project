7995a8a6a87cd2a6e684735b625f7e0131e4d643
Jarret Lovelace <jarretlovelace@icloud.com>
2024-08-09 11:43:51 -0600

    project submission

--- api/server.js
+++ api/server.js
@@ -4,6 +4,27 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+
+server.delete('/api/users/:id', async (req, res) => {
+try {
+ const possibleUser = await User.findById(req.params.id)
+if (!possibleUser) {
+    res.status(404).json({
+        message: "The user with the specified ID does not exist",
+    })
+} else {
+    const deletedUser = await User.remove(req.params.id)
+    res.status(200).json(deletedUser)
+} 
+} catch (err) {
+        res.status(500).json({
+            message: 'error deleting user',
+            err: err.message,
+            stack: err.stack, 
+})
+}
+})
+
 server.post('/api/users', (req, res) => {
     const user = req.body;
     if (!user.name || !user.bio) {
@@ -63,6 +84,34 @@ server.get('/api/users', (req, res) => {
     res.json('users')
 })
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist', 
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status (400).json({
+                    message: 'Please provide name and bio for the user', 
+                })
+            } else {
+              const updatedUser = await User.update(
+                req.params.id, 
+                req.body,
+            )
+              res.status(200).json(updatedUser)
+            }
+        }  
+    } catch (err) {
+        res.status(500).json({
+            messsage: 'error updating user', 
+            err: err.message, 
+            stack: err.stack, 
+        })
+      }
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({
